dfe226598e7359106ce13108464e330474381bf2
haenakimkim <147678434+haenakimkim@users.noreply.github.com>
2023-12-04 13:01:01 +0900

    객체 만드는법

---
+++ i.js
@@ -0,0 +1,21 @@
+//객체
+let codeit = {
+ 'n ame': '코드잇',
+ bornYear: 2017,
+ isVeryNice: true,
+  worstCourse: null,
+  bestCourse: {
+    title: '자바스크립트 프로그래밍 기초',
+    language: 'JavaScript'
+  }
+};
+
+//점 표기법
+console.log(codeit.bornYear);
+
+//대괄호 표기법
+console.log (codeit['n ame']);
+
+//복잡한 객체 표기법
+console.log(codeit.bestCourse.title);
+

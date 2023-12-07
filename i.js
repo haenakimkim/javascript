cbcee9b07cd4052121eb6fe4d3fbde1d4b04181b
haenakimkim <147678434+haenakimkim@users.noreply.github.com>
2023-12-07 10:54:25 +0900

    객체의 형변환

--- i.js
+++ i.js
@@ -1,8 +1,8 @@
-//시간
 
-let myDate =new Date();
+//Date객체의 형변환
+let myDate = new Date (2017, 4, 18);
 
-console.log(myDate.toLocaleDateString());
-console.log(myDate.toLocaleTimeString());
-console.log(myDate.toLocaleString());
-
+console.log(typeof myDate);
+console.log(String(myDate));
+console.log(Number(myDate));
+console.log(Boolean(myDate));

d9b0b10453fd8364b9f571ba55bccdee7cf0ead3
haenakimkim <147678434+haenakimkim@users.noreply.github.com>
2023-12-08 12:00:32 +0900

    배열의 메소드

---
+++ b.js
@@ -0,0 +1,11 @@
+//배열의 메소드 
+let members = ['쿤갈레', 'Zerrard', '우리생각해써', 
+'흙토끼', 'End Miracle'];
+console.log(members);
+
+//splice
+members.splice(1,1);
+console.log(members);
+
+members.splice(1,1,'upload');
+console.log(members);

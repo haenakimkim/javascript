ebc489728649a012a9a8686eb98caeca121b6609
haenakimkim <147678434+haenakimkim@users.noreply.github.com>
2023-12-12 11:20:42 +0900

    숫자 표기법 및 진법

---
+++ d.js
@@ -0,0 +1,19 @@
+//숫자표기법
+let millionare=1000000000;
+let myNumber= 1e9;
+console.log(millionare);
+console.log(myNumber);
+console.log(16e-5);
+
+//16진법
+let hex1=0xff;
+
+//8진법
+let octal = 0o377;
+
+//2진법
+let binary = 0b11111111;
+
+console.log (hex1);
+console.log (octal);
+console.log (binary);

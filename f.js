3c0cd4ebefb92eedc6dd896041820e3ac81bbb61
haenakimkim <147678434+haenakimkim@users.noreply.github.com>
2023-12-12 11:43:20 +0900

    진법 나타내기 /  Math

---
+++ f.js
@@ -0,0 +1,19 @@
+//진법 나타내기
+let myNumber =255;
+console.log(myNumber. toString(2));
+console.log(myNumber. toString (8));
+console.log(myNumber. toString(16));
+
+console.log(255..toString(2));
+console.log((255).toString(16));
+
+//Math 연산
+
+Math.abs(x) //절댓값
+Math.max/min //최대최솟값
+Math.pow(x,y)//x의 y승
+Math.sqrt(x)//x의 제곱근
+Math.round(x) //x의 반올림 값
+Math.floor(x)//x의 버림값
+Math.ceil(x)//x의 올림값
+Math.random //0이상 1미만 값 

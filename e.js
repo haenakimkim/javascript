64f48bac30278d844eb153c3d6b8f71a204e440f
haenakimkim <147678434+haenakimkim@users.noreply.github.com>
2023-12-12 11:31:10 +0900

    toFixed

---
+++ e.js
@@ -0,0 +1,15 @@
+//Number
+let myNumber = 0.3591;
+
+//toFixed(0~100)
+console.log(myNumber.toFixed(3));
+console.log(myNumber.toFixed(7));
+
+console.log(typeof myNumber.toFixed(7));
+
+console.log(typeof(myNumber.toFixed(7)));
+
+console.log(Number(myNumber.toFixed(7)));
+
+console.log(+myNumber.toFixed(2));
+

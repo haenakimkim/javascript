e60c6276f4e6e1089dada5dfd9f08f4c2868bb2e
haenakimkim <147678434+haenakimkim@users.noreply.github.com>
2023-12-13 11:37:51 +0900

    문자열

---
+++ h.js
@@ -0,0 +1,27 @@
+
+let myString='Hi Codeit'
+
+//문자열 길이
+console.log(myString.length);
+
+//요소접근
+console.log(myString[3]);
+console.log(myString.charAt(3));
+
+//요소탐색
+console.log(myString.indexOf('i'));
+console.log(myString.lastIndexOf('i'));
+
+//문자열에 없는 것 -1 
+
+//대문자 소문자 변환
+console.log(myString.toUpperCase());
+console.log(myString.toLowerCase());
+
+//양끝 공백제거
+console.log(myString.trim());
+
+//부분문자열 접근
+console.log(myString.slice(0,2));
+console.log(myString.slice(3));
+console.log(myString.slice());

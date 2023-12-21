472a4af4ef29455897ed278b187a95a165cff2a3
haenakimkim <147678434+haenakimkim@users.noreply.github.com>
2023-12-21 11:00:49 +0900

    AND OR 연산

---
+++ m.js
@@ -0,0 +1,9 @@
+//AND OR
+
+//AND && 왼 truthy -> 오른쪽 , 왼 Falsy->왼쪽
+console.log(0 && True)
+console.log('Codeit'&&'javascript')
+
+//OR || 왼 truty-> 왼쪽, 왼 Falsy-> 오른쪽
+console.log(null||undefined);
+console.log({} || 123)

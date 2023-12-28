b3ac671ab910b877ba0f7ed383900281cb91bb8e
haenakimkim <147678434+haenakimkim@users.noreply.github.com>
2023-12-28 11:03:41 +0900

    parameter

---
+++ p.js
@@ -0,0 +1,25 @@
+//parameter
+
+function greeting (name){
+  console.log(`Hi My name is ${name};`);
+}
+
+greeting ('javaScript');
+greeting ();
+
+
+
+function greeting (codeit){
+  console.log(`Hi My name is ${name};`);
+}
+
+greeting ();
+
+
+function greeting (name){
+  console.log(`Hi My name is ${name};`);
+  console.log ('I like ${interest};');
+}
+greeting('JavaScript');
+greeting(undefined,'phyton');
+

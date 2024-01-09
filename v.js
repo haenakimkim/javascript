e09c009382c8a8b58a46ebab6a28b53e6fad1378
haenakimkim <147678434+haenakimkim@users.noreply.github.com>
2024-01-09 10:52:05 +0900

    구조분해 객체

---
+++ v.js
@@ -0,0 +1,15 @@
+const rank = ['효준','유나','민환','재하'];
+
+//const macbook =rank[0];
+//const ipad= rank[1];
+//const airpods=rank[2];
+//const coupon= rank [3];
+
+const[macbook, ipad, airpods, coupon] =rank;
+
+console.log (macbook);
+console.log(ipad);
+console.log(airpods);
+console.log(coupon);
+
+

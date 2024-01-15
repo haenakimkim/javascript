fee5790bbc31eaef1798d6d87df20125b28b863e
haenakimkim <147678434+haenakimkim@users.noreply.github.com>
2024-01-15 10:15:50 +0900

    try cathc문

---
+++ w.js
@@ -0,0 +1,10 @@
+try{
+  console.log('에러 전');
+  const codeit='코드잇';
+  console.log(codeit);
+  codeit ='codeit';
+  const language='javascript';
+  console.log(language);
+}catch(error){
+  console.log('에러 후');
+  }

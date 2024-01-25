79b1ea43dd469e9e855bd60bd233effc755c87ad
haenakimkim <147678434+haenakimkim@users.noreply.github.com>
2024-01-25 10:09:06 +0900

    Add files via upload

---
+++ sort.js
@@ -0,0 +1,19 @@
+const number = [1, 10 ,4, 21, 36000];
+
+//오름차순 정렬
+numbers.sort((a,b)=>a-b);
+console.log(numbers);
+
+//내림차순 정렬
+numbers.sort((a,b)=>b-a);
+console.log(numbers);
+
+//reverse
+const letters = ['a', 'b', 'c'];
+const numbers = [427 ,721 ,353];
+
+letters.reverse();
+numbers.reverse();
+
+console.log(letters);
+console.log(numbers);

4817f761d3a0a4a0a755f828b60a7e3d8defe222
haenakimkim <147678434+haenakimkim@users.noreply.github.com>
2024-02-05 10:43:06 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,2 @@
-//문자>숫자
-let x='문자';
-console.log(x);
-console.log(Number (x));
-console.log(typeof Number(x));
-
-console.log('');
-
-//불린>숫자
-let y = true;
-console.log(y);
-console.log(Number (y));
-console.log(typeof y);
-console.log(typeof Number(y));
-
+import ( title, print ) from ''./모듈.js';
+print(title);
